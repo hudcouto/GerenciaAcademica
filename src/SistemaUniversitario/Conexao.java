8bf6d41fd1968875116c6d9958af1025b1809957
Luiz Santos <lsantosdutra@gmail.com>
2014-11-25 18:44:20 -0200

    Alterando conexao

---
+++ src/SistemaUniversitario/Conexao.java
@@ -0,0 +1,42 @@
+import java.beans.Statement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Luiz Santos
+ */
+public class Conexao 
+{
+    final private String driver = "com.mysql.jdbc.Driver";
+    final private String url = "jdbc:mysql://locahost/salaodebeleza";
+    final private String usuario = "usuario";
+    final private String senha = "senha";
+    private Connection conexao;
+    public Statement statement;
+    public ResultSet resultset;
+
+    public boolean conecta()
+    {
+        boolean result = true;
+        try
+        {
+            Class.forName(driver);
+            conexao = DriverManager.getConnection(url, usuario, senha);
+            JOptionPane.showMessageDialog(null, "Banco de Dados Conectado!");
+        }
+        catch(ClassNotFoundException Driver)
+        {
+            JOptionPane.showMessageDialog(null, "Driver não localizado: " + Driver);
+            result = false;
+        }
+        catch(SQLException Fonte)
+        {
+            JOptionPane.showMessageDialog(null, "Erro na conexão\n com a fonte de dados: \n" + Fonte);
+            result = false;
+        }
+        return result;
+    }
